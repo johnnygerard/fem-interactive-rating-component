384bbf337fcbb5f21742e7984a71725f752830f4
Johnny Gérard <john@jgerard.dev>
2024-05-24 01:57:21 +0200

    feat: add animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, model } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { SvgStarIconComponent } from "./svg/svg-star-icon/svg-star-icon.component";
 import { SvgOnlinePaymentIconComponent } from "./svg/svg-online-payment-icon/svg-online-payment-icon.component";
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: "app-root",
@@ -10,6 +11,14 @@ import { SvgOnlinePaymentIconComponent } from "./svg/svg-online-payment-icon/svg
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger("fadeIn", [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate("400ms ease-in", style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   isSubmitted = false;
