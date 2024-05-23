5feb0d1ba620aad825fa619d8a64567d004db466
Johnny Gérard <john@jgerard.dev>
2024-05-23 19:17:06 +0200

    feat: app-root template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, model } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { SvgStarIconComponent } from "./svg/svg-star-icon/svg-star-icon.component";
+import { SvgOnlinePaymentIconComponent } from "./svg/svg-online-payment-icon/svg-online-payment-icon.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [],
+  imports: [FormsModule, SvgStarIconComponent, SvgOnlinePaymentIconComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  isSubmitted = false;
+  userRating = model<null | number>(null);
+}
